d9998c03791a18b1cde5f44306fc29f8658ee171
Sasha <rustanovych@gmail.com>
2016-05-19 18:34:45 +0300

    Add toolbar

--- app/src/main/java/com/rustan/testtask/MainActivity.java
+++ app/src/main/java/com/rustan/testtask/MainActivity.java
@@ -2,12 +2,19 @@ package com.rustan.testtask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setLogo(R.mipmap.ic_launcher);
+        setSupportActionBar(toolbar);
     }
 }
