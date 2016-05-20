a3396c6b619b72d52357b87ee49169de75579475
Sasha <rustanovych@gmail.com>
2016-05-20 22:59:18 +0300

    Add new activity with toolbar

---
+++ app/src/main/java/com/rustan/testtask/ResultActivity.java
@@ -0,0 +1,28 @@
+package com.rustan.testtask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+public class ResultActivity extends AppCompatActivity {
+
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(),"Back button Click",Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+}
