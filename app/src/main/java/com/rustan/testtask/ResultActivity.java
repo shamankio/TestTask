bf6c58effc30e2fca2d68a2eee81fdb3e45f61c1
Sasha <rustanovych@gmail.com>
2016-05-21 00:47:07 +0300

    Add menu

--- app/src/main/java/com/rustan/testtask/ResultActivity.java
+++ app/src/main/java/com/rustan/testtask/ResultActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class ResultActivity extends AppCompatActivity {
 
@@ -27,5 +30,28 @@ public class ResultActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_result_activity,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.openBrowser:
+                Toast.makeText(getApplicationContext(),"Open browser",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.save:
+                Toast.makeText(getApplicationContext(),"Save",Toast.LENGTH_LONG).show();
+                break;
+            case R.id.share:
+                Toast.makeText(getApplicationContext(),"Share",Toast.LENGTH_LONG).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
