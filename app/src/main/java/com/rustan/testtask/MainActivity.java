09193f1c4962dbda9c5e2efa72ce306d296cf4bf
Sasha <rustanovych@gmail.com>
2016-05-20 22:40:12 +0300

    First page layout

--- app/src/main/java/com/rustan/testtask/MainActivity.java
+++ app/src/main/java/com/rustan/testtask/MainActivity.java
@@ -1,12 +1,16 @@
 package com.rustan.testtask;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,10 @@ public class MainActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
         setSupportActionBar(toolbar);
+        editText = (EditText)findViewById(R.id.editText);
+    }
+    public void clickButton(View view){
+        String name = editText.getText().toString();
+        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
     }
 }
