b94cb4588d8dc9cf7a17ae76537b773bdc7144fa
Sasha <rustanovych@gmail.com>
2016-05-20 23:44:35 +0300

    Add new activity with toolbar

--- app/src/main/java/com/rustan/testtask/MainActivity.java
+++ app/src/main/java/com/rustan/testtask/MainActivity.java
@@ -1,5 +1,6 @@
 package com.rustan.testtask;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
     private EditText editText;
+    public static final String NAME = "NAME";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +21,16 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setLogo(R.mipmap.ic_launcher);
+        toolbar.setTitle(R.string.app_name);
         setSupportActionBar(toolbar);
-        editText = (EditText)findViewById(R.id.editText);
+        editText = (EditText) findViewById(R.id.editText);
     }
-    public void clickButton(View view){
+
+    public void clickButton(View view) {
         String name = editText.getText().toString();
-        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_LONG).show();
+        Toast.makeText(getApplicationContext(), name, Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(this, ResultActivity.class);
+        intent.putExtra(NAME, name);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/rustan/testtask/ResultActivity.java
+++ app/src/main/java/com/rustan/testtask/ResultActivity.java
@@ -1,7 +1,8 @@
 package com.rustan.testtask;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Toast;
@@ -9,20 +10,24 @@ import android.widget.Toast;
 public class ResultActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+        name = getIntent().getStringExtra(MainActivity.NAME);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(name);
         toolbar.setNavigationIcon(R.drawable.ic_arrow_back_black_24dp);
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"Back button Click",Toast.LENGTH_LONG).show();
+                Toast.makeText(getApplicationContext(), "Back button Click", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
             }
         });
-
     }
 }
